4a130d00b35d95598df16e3f880ce78703c2ddca
juannodeb <juannodeb@gmail.com>
2020-06-06 12:31:55 -0500

    Added and installed axios and made the same requests as with the request dependency

--- 7_JS_CRUD/main.js
+++ 7_JS_CRUD/main.js
@@ -4,6 +4,7 @@
 // D --> delete
 
 const request = require('request');
+const axios = require('axios');
 const mainEndpoint = 'https://goodreads-devf-aaron.herokuapp.com/api/v1/authors/';
 
 /*
@@ -12,6 +13,7 @@ const mainEndpoint = 'https://goodreads-devf-aaron.herokuapp.com/api/v1/authors/
      nuevo en una promesa.
 */
 
+// REQUEST
 const createAuthor = (name, lastName, bio, gender, age) => {
   const author = {
     name: name,
@@ -39,12 +41,32 @@ const createAuthor = (name, lastName, bio, gender, age) => {
 //   .then((author) => console.log(author))
 //   .catch((error) => console.log(error));
 
+// AXIOS
+const axiosCreateAuthor = (name, lastName, bio, gender, age) => {
+  const author = {
+    name: name,
+    last_name: lastName,
+    nacionalidad: 'MX',
+    biography: bio,
+    gender: gender,
+    age: age,
+    is_alive: true
+  };
+
+  return axios.post(mainEndpoint, author);
+}
+
+// axiosCreateAuthor('Paco', 'Paco', 'Hola, soy Paco', 'M', 23)
+//   .then(({ data }) => console.log(data)) // Se coloca { data } para deconstruir la respuesta que me llega y mostrar sólo el valor de la llave 'data'
+//   .catch((error) => console.log(error));
+
 /*
   2. Escribe una función que reciba como parámetro un ID para realizar una petición a la API
      para hallar un autor con el ID especificado. La función debe retornar una promesa con
      la respuesta del servidor.
 */
 
+// REQUEST
 const getAuthorByID = (id) => {
   const endpoint = mainEndpoint + id;
 
@@ -64,12 +86,24 @@ const getAuthorByID = (id) => {
 //   .then((author) => console.log(author))
 //   .catch((error) => console.log(error));
 
+// AXIOS
+const axiosGetAuthorByID = (id) => {
+  const endpoint = mainEndpoint + id;
+
+  return axios.get(endpoint);
+}
+
+// axiosGetAuthorByID(6669)
+//   .then(({ data }) => console.log(data))
+//   .catch((error) => console.log(error));
+
 /*
   3. Escribe una función que reciba como parámetros nombre, nacionalidad, biografía, género y edad
      para hacer una petición a la API para modificar un autor existente. Al ejecutar La función,
      esta debe retornar el autor modificado en una promesa.
 */
 
+// REQUEST
 const updateAuthor = (id, name, lastName, bio, gender, age) => {
   const endpoint = mainEndpoint + id + '/';
   const author = {
@@ -99,12 +133,34 @@ const updateAuthor = (id, name, lastName, bio, gender, age) => {
 //   .then((author) => console.log(author))
 //   .catch((error) => console.log(error));
 
+// AXIOS
+const axiosUpdateAuthor = (id, name, lastName, bio, gender, age) => {
+  const endpoint = mainEndpoint + id + '/';
+  const author = {
+    id: id,
+    name: name,
+    last_name: lastName,
+    nacionalidad: 'MX',
+    biography: bio,
+    gender: gender,
+    age: age,
+    is_alive: true
+  }
+
+  return axios.put(endpoint, author);
+};
+
+// axiosUpdateAuthor(6669, 'Otro Paco', 'Otro Paco', 'Esta es mi otra bio', 'M', 32)
+//   .then(({ data }) => console.log(data))
+//   .catch((error) => console.log(error));
+
 /*
   4. Escribe una función que reciba como parámetro un ID para realizar una petición a la API para
      eliminar un autor con el ID especificado. La función debe retornar una promesa con la
      respuesta del servidor.
 */
 
+// REQUEST
 const deleteAuthorByID = (id) => {
   const endpoint = mainEndpoint + id + '/';
 
@@ -123,6 +179,17 @@ const deleteAuthorByID = (id) => {
 //   .then((author) => console.log(author))
 //   .catch((error) => console.log(error));
 
+// AXIOS
+const axiosDeleteAuthorByID = (id) => {
+  const endpoint = mainEndpoint + id + '/';
+
+  return axios.delete(endpoint);
+}
+
+// axiosDeleteAuthorByID(6671)
+//   .then((data) => console.log(`${data.status} - Autor eliminado correctamente`))
+//   .catch((error) => console.log(error));
+
 /*
   5. Encadenando promesas, es decir, usando las funciones que escribiste en los puntos previos,
      debes: crear, modificar y eliminar un autor.
@@ -130,38 +197,38 @@ const deleteAuthorByID = (id) => {
 
 
 // CALLBACK HELL D:
-createAuthor('Juan', 'Juan', 'Esta es mi bio', 'M', 23)
-  .then((createdAuthor) => {
-    console.log('Autor creado');
-    console.log(createdAuthor);
+// createAuthor('Juan', 'Juan', 'Esta es mi bio', 'M', 23)
+//   .then((createdAuthor) => {
+//     console.log('Autor creado');
+//     console.log(createdAuthor);
 
-    getAuthorByID(createdAuthor.id)
-      .then((fetchedAuthor) => {
-        console.log('Autor encontrado');
-        console.log(fetchedAuthor);
+//     getAuthorByID(createdAuthor.id)
+//       .then((fetchedAuthor) => {
+//         console.log('Autor encontrado');
+//         console.log(fetchedAuthor);
 
-        updateAuthor(fetchedAuthor.id, 'Otro Juan', 'Otro Juan', 'Otra bio', 'M', 24)
-          .then((updatedAuthor) => {
-            console.log('Autor actualizado');
-            console.log(updatedAuthor);
+//         updateAuthor(fetchedAuthor.id, 'Otro Juan', 'Otro Juan', 'Otra bio', 'M', 24)
+//           .then((updatedAuthor) => {
+//             console.log('Autor actualizado');
+//             console.log(updatedAuthor);
 
-            deleteAuthorByID(fetchedAuthor.id)
-              .then((deletedAuthor) => {
-                console.log('Autor eliminado');
-                console.log(deletedAuthor);
-              })
-              .catch((deletedAuthorError) => {
-                console.log(`${deletedAuthorError} - Error al eliminar el autor`)
-              })
-          })
-          .catch((updatedAuthorError) => {
-            console.log(`${updatedAuthorError} - Error al actualizar el autor`)
-          })
-      })
-      .catch((fetchedAuthorError) => {
-        console.log(`${fetchedAuthorError} - Error al obtener el autor`)
-      })
-  })
-  .catch((createdAuthorError) => {
-    console.log(`${createdAuthorError} - Error al crear el autor`)
-  });
+//             deleteAuthorByID(fetchedAuthor.id)
+//               .then((deletedAuthor) => {
+//                 console.log('Autor eliminado');
+//                 console.log(deletedAuthor);
+//               })
+//               .catch((deletedAuthorError) => {
+//                 console.log(`${deletedAuthorError} - Error al eliminar el autor`)
+//               })
+//           })
+//           .catch((updatedAuthorError) => {
+//             console.log(`${updatedAuthorError} - Error al actualizar el autor`)
+//           })
+//       })
+//       .catch((fetchedAuthorError) => {
+//         console.log(`${fetchedAuthorError} - Error al obtener el autor`)
+//       })
+//   })
+//   .catch((createdAuthorError) => {
+//     console.log(`${createdAuthorError} - Error al crear el autor`)
+//   });
